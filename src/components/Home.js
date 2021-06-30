2bf6ae6501ea27a9c3407a2bbfd0144a94253f3d
Jiashu <j832wang@uwaterloo.ca>
2021-06-30 00:45:30 -0600

    added custom dictionary, skips and proper pinyin accents

--- src/components/Home.js
+++ src/components/Home.js
@@ -39,6 +39,51 @@ const LetterContainer = styled.div`
 
 const NBSP = '\u00a0'
 
+class DictionaryStore {
+    data = {};
+    constructor() { }
+
+    add(key, value) {
+        if (!this.data[key]) {
+            this.data[key] = []
+        }
+        this.data[key].push(value)
+    }
+
+    get(key) {
+        console.log(this.data[key])
+        return this.data[key][0]
+    }
+
+    contains(key, value) {
+        if (this.data[key]) {
+            for (let c of this.data[key]) {
+                console.log("comparing " + c + " and " + value)
+                if (value === c) {
+                    return true;
+                }
+            }
+        }
+        return false
+    }
+
+    //returns a boolean indicating whether or not the entire key is gone
+    remove(key, value) {
+        if (this.data[key]) {
+            this.data[key] = this.data[key].filter(function (v, index, arr) {
+                return value != v
+            });
+
+            if (this.data[key].length == 0) {
+                delete this.data[key]
+                return true;
+            }
+        }
+        return false
+    }
+
+};
+
 export class Home extends React.Component {
     constructor(props) {
         super(props);
@@ -50,23 +95,54 @@ export class Home extends React.Component {
                 }
             ],
         };
+        
         let data = require('./data/data.json');
+        let custom = require('./data/custom.json');
+        let skip = require('./data/skip.json');
         console.log(data)
+        console.log(custom)
+        console.log(skip)
 
         this.trie = new Trie();
-        this.dict = {};
-        //todo: 多音字. currently, dict just gets overwritten, so 
-        // i think it's currently the most unpopular 多音字.
-        // maybe make a queue? add to the end, and when we fetch,
-        // we want the first (hopefully most common).
-        //  TODO: possible new feature? have user correct it? 
+        // priority of data dict is FILO
+        this.dict = new DictionaryStore();
+        
+        // Note: 多音字. currently, dict maps simplified -> list of pinyins.
+        // When fetching, we simply take the first element in list.
+        // (which is hopefully the most common)
+        
+        // To prioritize something, currently just add it to custom.
+        // TODO: think of a better way to handle this?
+        // TODO: possible new feature? have user correct it? 
         //      with our current infrastructure, we can add 
-        //      surrounding words into the dict, and it can remember
-        //      that way?
+        //      surrounding words into the custom automatically,
+        //      and it can remember that way?
+
+        //add custom first
+        custom.forEach(entry => {
+            let simplified = entry["simplified"]
+            this.trie.insert(simplified)
+            this.dict.add(simplified, entry["pinyin"]);
+        });
 
         data.forEach(entry => {
-            this.trie.insert(entry["simplified"])
-            this.dict[entry["simplified"]] = entry["pinyin"];
+            let simplified = entry["simplified"]
+            this.trie.insert(simplified)
+            this.dict.add(simplified, entry["pinyin"]);
+        });
+
+        skip.forEach(entry => {
+            let simplified = entry["simplified"]
+            let pinyin = entry["pinyin"]
+            this.trie.insert(simplified)
+            if (this.dict.contains(simplified, pinyin)) {
+                //If completely removed from dict, remove from trie
+                if (this.dict.remove(simplified, pinyin)) {
+                    this.trie.remove(simplified);
+                }
+            } else {
+                console.error("Unrecognized skip");
+            }
         });
 
         this.vowels = require('./data/vowels.json');
@@ -84,26 +160,59 @@ export class Home extends React.Component {
             return ""
         }
 
+        //special case with no vowel
+        if (pinyin == "r5") {
+            return "r";
+        }
+
         let accent = pinyin[pinyin.length - 1];
         var word = pinyin.substr(0, pinyin.length - 1);
 
-        // TODO: accent priority should be in the order aoeiuü
+        if (accent == "5") { // 5 should be 轻声
+            return word;
+        }
+
+        // Note: accent priority should be in the order aoeiuü
         // Note: in the case of 'iu' or 'ui', accent goes onto the terminal
         //      Ex. liú or guǐ
         // source: http://www.ichineselearning.com/learn/pinyin-tones.html
-        
-        //replaces first vowel encountered.
-        for (var i = 0; i < pinyin.length; i++) {
-            let char = pinyin[i].toLowerCase()
-            // ü is written as u: in ascii
-            if (char == "u" && pinyin[i + 1] == ":") {
-                char = 'u:'
-            }
 
-            if (this.vowels[char]) {
-                return word.replace(char, this.vowels[char][accent])
-            }
+        var char = "";
+        if (word.includes("a")) {
+            char = "a"
+        } else if (word.includes("o")) {
+            char = "o"
+        } else if (word.includes("e")) {
+            char = "e"
+        } else if (word.includes("iu")) {
+            char = "u"
+        } else if (word.includes("ui")) {
+            char = "i"
+        } else if (word.includes("i")) {
+            char = "i"
+        } else if (word.includes("u:")) {
+            // confirmed by hand that u and u: don't appear in the same word
+            char = "u:"
+        } else if (word.includes("u")) {
+            char = "u"
+        } else {
+            console.error("found pinyin with no vowel: " + pinyin);
         }
+
+        if (this.vowels[char]) {
+            return word.replace(char, this.vowels[char][accent])
+        }
+        // for (var i = 0; i < word.length; i++) {
+        //     let char = word[i].toLowerCase()
+        //     // ü is written as u: in ascii
+        //     if (char == "u" && word[i + 1] == ":") {
+        //         char = 'u:'
+        //     }
+
+        //     if (this.vowels[char]) {
+        //         return word.replace(char, this.vowels[char][accent])
+        //     }
+        // }
         return word
     }
 
@@ -130,17 +239,18 @@ export class Home extends React.Component {
         console.log("===============================")
         console.log(text)
 
+        //TODO: make splitting into verses better...
         // Fragments? Verses?
         // replace all '.'
         var fragments = text.split('。');
-        for(var i = 0; i < fragments.length - 1; i++){
+        for (var i = 0; i < fragments.length - 1; i++) {
             fragments[i] += '。'
         }
-
+ 
         //replace all ','
-        for(var i = 0; i < fragments.length; i++){
+        for (var i = 0; i < fragments.length; i++) {
             var fragment2 = fragments[i].split(',');
-            for(var j = 0; j < fragment2.length -1; j++) {
+            for (var j = 0; j < fragment2.length - 1; j++) {
                 fragment2[j] = ','
             }
         }
@@ -172,6 +282,7 @@ export class Home extends React.Component {
                     continue; // no more chinese chars in fragment, next pls
                 }
 
+                //findBest is simply greedy algo, find the longest
                 var phrase = this.trie.findBest(partial)
                 console.log("phrase: " + phrase);
                 if (phrase == "") {
@@ -180,7 +291,7 @@ export class Home extends React.Component {
                 } else {
                     //found something in trie, remove from partial
                     partial = partial.substr(phrase.length, partial.length);
-                    let pinyin = this.dict[phrase].split(" ");
+                    let pinyin = this.dict.get(phrase).split(" ");
                     if (phrase.length != pinyin.length) {
                         console.error("pinyin and phrase have different lengths O.o")
                         console.error("phrase: " + phrase)
